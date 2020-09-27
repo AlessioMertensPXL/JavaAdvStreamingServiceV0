cbba1e1d5184837b8a9bed084d0acb3ab50b09a6
AlessioMertensPXL <Alessio.Mertens@STUDENT.PXL.BE>
2020-09-27 14:51:09 +0200

    passwordstrength testen

--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -1,5 +1,27 @@
 package be.pxl.ja.streamingservice.util;
 
-public class PasswordUtilCalculateStrengthTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordUtilCalculateStrengthTest {
+    @Test
+    public void PasswordStrengthMatchesConditions() {
+        StringBuilder password = new StringBuilder();
+        password.append("aaa");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()), 0);
+        password.append("aaa");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 2, 1);
+        password.append("aaaaaaaaa");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 2, 2);
+        password.delete(0, password.length());
+        password.append("123456");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+        password.delete(0, password.length());
+        password.append("DDDDDD");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+        password.delete(0, password.length());
+        password.append("~!@#$%");
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+    }
 }
