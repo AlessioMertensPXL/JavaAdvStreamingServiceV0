bad2d9d1c5533eb5d105145cd877737d2f8ca643
AlessioMertensPXL <Alessio.Mertens@STUDENT.PXL.BE>
2020-09-29 15:36:52 +0200

    passwordstrength testen

--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -7,21 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PasswordUtilCalculateStrengthTest {
     @Test
     public void PasswordStrengthMatchesConditions() {
+        int lengthBasedStrength = 1;
+        int lowerCaseBasedStrength = 2;
         StringBuilder password = new StringBuilder();
         password.append("aaa");
         assertEquals(PasswordUtil.calculateStrength(password.toString()), 0);
         password.append("aaa");
-        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 2, 1);
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - lowerCaseBasedStrength, 1);
         password.append("aaaaaaaaa");
-        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 2, 2);
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - lowerCaseBasedStrength, 2);
         password.delete(0, password.length());
         password.append("123456");
-        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - lengthBasedStrength, 2);
         password.delete(0, password.length());
         password.append("DDDDDD");
-        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - lengthBasedStrength, 2);
         password.delete(0, password.length());
         password.append("~!@#$%");
-        assertEquals(PasswordUtil.calculateStrength(password.toString()) - 1, 2);
+        assertEquals(PasswordUtil.calculateStrength(password.toString()) - lengthBasedStrength, 2);
     }
 }
